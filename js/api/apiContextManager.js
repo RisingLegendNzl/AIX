b0efcdf859bbcb75c8ddfc0a93a60d16e2db8f3f
LEGEND <risinglegendnzl@gmail.com>
2025-12-18 22:55:12 +1000

    Update apiContextManager.js

--- js/api/apiContextManager.js
+++ js/api/apiContextManager.js
@@ -10,6 +10,50 @@ class ApiContextManager {
         this.lastSpin = null;
         this.lastApiResponse = null;
         this.pollingIntervalMs = 2500; // 2.5 seconds
+        this.autoModeEnabled = false; // NEW: Track auto mode state
+    }
+
+    /**
+     * Enables auto mode (API becomes input source)
+     */
+    enableAutoMode() {
+        this.autoModeEnabled = true;
+    }
+
+    /**
+     * Disables auto mode (manual input enabled)
+     */
+    disableAutoMode() {
+        this.autoModeEnabled = false;
+        this.stopLivePolling(); // Stop polling when disabling auto mode
+    }
+
+    /**
+     * Sets auto mode state (convenience method for toggle)
+     * @param {boolean} enabled - True to enable auto mode, false to disable
+     */
+    setAutoMode(enabled) {
+        if (enabled) {
+            this.enableAutoMode();
+        } else {
+            this.disableAutoMode();
+        }
+    }
+
+    /**
+     * Checks if auto mode is currently enabled
+     * @returns {boolean} True if auto mode is enabled
+     */
+    isAutoModeEnabled() {
+        return this.autoModeEnabled;
+    }
+
+    /**
+     * Checks if currently in live polling mode (alias for isPolling)
+     * @returns {boolean} True if live polling is active
+     */
+    isLivePollingActive() {
+        return this.isLivePolling;
     }
 
     /**
@@ -112,6 +156,7 @@ class ApiContextManager {
         this.currentTable = null;
         this.lastSpin = null;
         this.lastApiResponse = null;
+        this.autoModeEnabled = false; // Reset auto mode
     }
 }
 
